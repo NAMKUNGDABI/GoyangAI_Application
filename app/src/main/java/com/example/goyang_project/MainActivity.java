00712ec3730eca985b72433f6fe00aa4a0b3f798
NAMKUNGDABI <nkdb98@naver.com>
2022-02-07 11:20:55 +0900

    locations select spinner

--- app/src/main/java/com/example/goyang_project/MainActivity.java
+++ app/src/main/java/com/example/goyang_project/MainActivity.java
@@ -17,30 +17,109 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button btn1;
+    Button predict_btn, loc_btn;
+    private Spinner spinnerGu, spinnerDong;
+    private ArrayAdapter<String> arrayAdapter;
+    public static final String EXTRA_ADDRESS = "address";
+    TextView cur_loc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = findViewById(R.id.button1);
+        spinnerGu = (Spinner)findViewById(R.id.loc_gu);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, (String[])getResources().getStringArray(R.array.goyangsi));
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGu.setAdapter(arrayAdapter);
+        spinnerDong = (Spinner)findViewById(R.id.loc_dong);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        initAddressSpinner();
+
+        loc_btn = findViewById(R.id.save_loc);
+        loc_btn.setOnClickListener(this);
+
+        predict_btn = findViewById(R.id.predict);
+        predict_btn.setOnClickListener(this);
+
+        cur_loc = findViewById(R.id.cur_loc);
+
+/*        predict_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(),PhotoActivity.class);
                 startActivity(intent);
             }
         });
+*/
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.save_loc) {
+            String address = "고양시 " + spinnerGu.getSelectedItem().toString() + " " + spinnerDong.getSelectedItem().toString();
+            cur_loc.setText("현재 위치 : " + address);
+
+        }
+        else if(view.getId() == R.id.predict){
+            Intent intent = new Intent(getApplicationContext(),PhotoActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void initAddressSpinner() {
+        spinnerGu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        setSigunguSpinnerAdapterItem(R.array.duckyanggu);
+                        break;
+                    case 1:
+                        setSigunguSpinnerAdapterItem(R.array.ilsanseogu);
+                        break;
+                    case 2:
+                        setSigunguSpinnerAdapterItem(R.array.ilsandonggu);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
+
+    private void setSigunguSpinnerAdapterItem(int array_resource) {
+        if (arrayAdapter != null) {
+            spinnerDong.setAdapter(null);
+            arrayAdapter = null;
+        }
+
+        if (spinnerGu.getSelectedItemPosition() > 1) {
+            spinnerDong.setAdapter(null);
+        }
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, (String[])getResources().getStringArray(array_resource));
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDong.setAdapter(arrayAdapter);
+    }
+
 }
